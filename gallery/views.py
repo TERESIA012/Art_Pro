65a4b0ed96e97ccdace94dde0b99f749d2e70454
TERESIA012 <kingoriteresia@gmail.com>
2021-10-08 00:12:11 +0300

    add index function

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    images = Image.objects.all()
+    locations = Location.get_locations()
+    print(locations)
+    return render(request, 'photo/index.html', {'images': images[::-1], 'locations': locations})
