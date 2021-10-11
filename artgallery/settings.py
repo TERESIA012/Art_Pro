6e12849315615ae3cbed767001176dc8bd5dd1b2
TERESIA012 <kingoriteresia@gmail.com>
2021-10-11 20:35:58 +0300

    deployment to herokiu

--- artgallery/settings.py
+++ artgallery/settings.py
@@ -26,7 +26,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY='django-insecure-)@qufkza&vz8k4(jddfl&yobuk#(7azenp1j)b)zg5%gj-@=l3'
 
 # setup cloudinary credentials for django-cloudinary
 cloudinary.config(
