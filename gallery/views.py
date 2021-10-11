c88feec06ed62f9ad1d5e635e28d8aaf10803ea2
TERESIA012 <kingoriteresia@gmail.com>
2021-10-11 15:33:44 +0300

    add location search function

--- gallery/views.py
+++ gallery/views.py
@@ -11,6 +11,18 @@ def index(request):
     return render(request, 'gallery/index.html', {'images': images[::-1], 'locations': locations})
 
 
+
+def search_location(request, location_id):
+    locations = Location.objects.all()
+    images = Image.objects.filter(location_id=location_id)
+    
+    location = Location.objects.get(id=location_id)
+    title = location
+    message = f"{location}"
+    # print(searched_media)
+    return render(request, 'gallery/location.html', {'images': images, 'locations': locations, 'message': message,'title':title})
+
+
 def search_results(request):
     if 'imagesearch' in request.GET and request.GET["imagesearch"]:
         category = request.GET.get("imagesearch")
